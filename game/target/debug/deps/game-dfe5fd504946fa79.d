e40afc5787813033b7e81674a13354c587107e23
Joshua Ogbonna <devjaykes@gmail.com>
2023-05-07 12:48:48 +0100

    feat: step five

---
+++ game/target/debug/deps/game-dfe5fd504946fa79.d
@@ -0,0 +1,5 @@
+/home/promshub/Desktop/Learns/rust/game/target/debug/deps/game-dfe5fd504946fa79: src/main.rs
+
+/home/promshub/Desktop/Learns/rust/game/target/debug/deps/game-dfe5fd504946fa79.d: src/main.rs
+
+src/main.rs:
---
+++ game/target/debug/game.d
@@ -0,0 +1,1 @@
+/home/promshub/Desktop/Learns/rust/game/target/debug/game: /home/promshub/Desktop/Learns/rust/game/src/main.rs
